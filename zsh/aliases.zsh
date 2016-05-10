b2ad1643b4b89d98be028efd660087857b3886e2
mindstorms6 <breland@bdawg.org>
2016-05-09 23:22:37 -0700

    Updates for ubi key

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,4 +4,6 @@ if [ ! $(uname -s) = "Darwin" ]; then
     alias pbcopy='xsel --clipboard --input'
     alias pbpaste='xsel --clipboard --output'
 fi
+export OPENSC_LIBS=/usr/lib/x86_64-linux-gnu/
+alias ussh='ssh -I $OPENSC_LIB/opensc-pkcs11.so'
 alias cls='clear' # Good 'ol Clear Screen command
